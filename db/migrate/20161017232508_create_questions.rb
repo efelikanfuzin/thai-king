8b31c027a08114e986604ec31afeac5c6d2f318c
Klimov Aleksandr <efelikanfuzin@gmail.com>
2016-10-20 01:39:03 +0300

    Fix db migrate

--- db/migrate/20161017232508_create_questions.rb
+++ db/migrate/20161017232508_create_questions.rb
@@ -2,8 +2,6 @@ class CreateQuestions < ActiveRecord::Migration[5.0]
   def change
     create_table :questions do |t|
       t.string :text
-      t.references :right_answer, foreign_key: true
-
       t.timestamps
     end
   end
